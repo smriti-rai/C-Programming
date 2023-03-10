d6ebeea1d4744f61ba034de360d768b051a3d647
shyam <smriti.rai1133@gmail.com>
2023-03-11 02:27:00 +0545

    Added usd.c

---
+++ usd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+   float n,u;
+   printf("US Dollars=");
+   scanf("%f",&u);
+   n=u*132.33;
+   printf("Nepalese Rupees=%f",n);
+   getchar();
+}
